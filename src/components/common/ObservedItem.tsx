138dc07ce41b9211034d837d583c1f97e375192a
euparkn <euparkn@gmail.com>
2023-09-04 21:02:34 +0900

    feat: useIntersectionObserver

---
+++ src/components/common/ObservedItem.tsx
@@ -0,0 +1,25 @@
+import { ReactNode, useRef } from "react";
+import { styled } from "styled-components";
+
+import { useIntersectionObserver } from "hooks/useIntersectionObserver";
+
+function ObservedItem({ children }: { children?: ReactNode }) {
+  const ref = useRef<HTMLDivElement>(null);
+  const { ratio } = useIntersectionObserver(ref, {
+    threshold: Array(10)
+      .fill(0)
+      .map((el, i) => i / 10),
+  });
+
+  return (
+    <ObservedBox ref={ref} style={{ opacity: ratio }}>
+      {children}
+    </ObservedBox>
+  );
+}
+
+const ObservedBox = styled.div`
+  background-color: #aaa;
+`;
+
+export default ObservedItem;
