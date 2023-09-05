874ce36993a63c885397c5ef150384e8db483405
euparkn <euparkn@gmail.com>
2023-09-05 21:08:20 +0900

    fix: params option

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,16 +1,24 @@
 import axios from "axios";
 import { API } from "api/apiConfig";
+import { PostDataDTO } from "types/type";
 
 const ACCESS_TOKEN = process.env.REACT_APP_ACCESS_TOKEN;
 const BLOG_URL = process.env.REACT_APP_BLOG_URL;
 
-// export const getPostList = async ({ page }: { page: string }) => {
+// export const getPostList = async ({
+//   page,
+//   count,
+// }: {
+//   page: string;
+//   count: string;
+// }) => {
 //   const response = await API.get(`/apis/post/list`, {
 //     params: {
 //       access_token: ACCESS_TOKEN,
 //       blogName: BLOG_URL,
 //       output: "json",
 //       page,
+//       count,
 //     },
 //     headers: {
 //       "Content-Type": `application/json; charset=UTF-8`,
@@ -20,7 +28,13 @@ const BLOG_URL = process.env.REACT_APP_BLOG_URL;
 //   return response.data;
 // };
 
-export const getPostList = async ({ page }: { page: string }) => {
+export const getPostList = async ({
+  page,
+  count,
+}: {
+  page: string;
+  count: string;
+}) => {
   const response = await axios.get(`/mock/mock.json`);
   return response.data;
 };
