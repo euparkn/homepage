8aa0cdf522c5af08feddfceb16e894de06080c8d
euparkn <euparkn@gmail.com>
2023-09-09 15:18:29 +0900

    feat: copyright

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,13 +1,27 @@
 import { styled } from "styled-components";
 
+const NAME = process.env.REACT_APP_NAME;
+
 function Footer() {
-  return <Wrapper>Footer</Wrapper>;
+  return (
+    <Container>
+      <small>© 2023 {NAME}</small>
+    </Container>
+  );
 }
 
 export default Footer;
 
-const Wrapper = styled.div`
+const Container = styled.footer`
   width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   height: 400px;
   background-color: #eee;
+  small {
+    font-weight: 700;
+    font-size: 0.85rem;
+    color: #777;
+  }
 `;
