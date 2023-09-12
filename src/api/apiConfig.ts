b854288e067ecb0e77dd7b650721485b7c53eac5
euparkn <euparkn@gmail.com>
2023-09-12 23:50:22 +0900

    build: add netlify proxy

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+// const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = "/proxy";
 
 export const API = axios.create({
   baseURL: `${BASE_URL}`,
