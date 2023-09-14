af2355c34f10d5906ad92a37a42b06dcaf656f84
euparkn <euparkn@gmail.com>
2023-09-14 20:58:57 +0900

    style: transition and font

--- src/components/common/ObservedItem.tsx
+++ src/components/common/ObservedItem.tsx
@@ -17,7 +17,7 @@ function ObservedItem({ children }: { children?: ReactNode }) {
 const ObservedBox = styled.div<{ $isInView: boolean }>`
   height: 100%;
   opacity: ${(props) => (props.$isInView ? 1 : 0)};
-  transition: 3s;
+  transition: 2s ease-in-out;
 `;
 
 export default ObservedItem;
